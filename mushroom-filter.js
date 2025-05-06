b3de5703dc67b5dee89ed28a5f0a082eeaffeb40
mon.ptell <mon.medialab@gmail.com>
2025-05-07 01:18:20 +0200

    filtering cards

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -8,26 +8,32 @@ const currentFilters = {
     edible: "all"
 };
 
-// refactoring with a function -----------------------------
-
-/* seasonalFilter.addEventListener("change", (event) => {
-    currentFilters.season = event.target.value;
-    console.log(currentFilters.season);
-});
-
-edibleFilter.addEventListener("change", (event) => {
-    currentFilters.edible = event.target.value;
-    console.log(currentFilters.edible);
-}); */
-
 seasonalFilter.addEventListener("change", updateFilter);
 edibleFilter.addEventListener("change", updateFilter);
 
 function updateFilter(e) {
-    //console.log(e.target.value);
-    //console.log(e.target.name);
     const filterType = e.target.name;
-    //console.log('filterType: ' + filterType);
     currentFilters[filterType] = e.target.value;
-    //console.log('currentFilters: ', currentFilters);
+
+    filterCards();
 }
+
+function filterCards() {
+    cards.forEach((card) => {
+        const season = card.querySelector("[data-season]").dataset.season;
+        const edible = card.querySelector("[data-edible]").dataset.edible;
+
+        const matchesSeason = currentFilters.season === season;
+        const matchesEdible = currentFilters.edible === edible;
+
+        if (
+            (matchesEdible || currentFilters.edible === "all") &&
+            (matchesSeason || currentFilters.season === "all")
+        ) {
+            card.hidden = false;
+        } else {
+            card.hidden = true;
+        }
+
+    });
+}   
